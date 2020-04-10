50fd4266ade341f127f300072e495a59c04ba907
Azula21 <janelinw@usc.edu>
2020-04-10 13:50:02 -0700

    Testing access to Heroku

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Main endpoint. Currently for testing."""
-    return "<h1>SenseHealth</h1>"
+    return "<h1>Welcome to SenseHealth's Backend</h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
