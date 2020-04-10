d44b8c0fc0de533af1984cb692879632125358ea
Adithya Bellathur <bellathu@usc.edu>
2020-04-10 13:37:56 -0700

    Set Up Flask Server * Main server sits in app.py * code that adds functionality to be added to src/ * runtime.txt added to specify python-3.7.7

---
+++ app.py
@@ -0,0 +1,14 @@
+"""The main application that runs flask server."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Main endpoint. Currently for testing."""
+    return "<h1>SenseHealth</h1>"
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
