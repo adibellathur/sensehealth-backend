9e156183078b7367f6a22f4556fade637b3e6d26
Adithya Bellathur <bellathu@usc.edu>
2020-04-10 19:14:12 -0700

    Added deposit_data Endpoint for Sensor Testing

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 """The main application that runs flask server."""
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
+@app.route('/deposit_data', methods=['POST'])
+def deposit_data():
+    """Temporary endpoint to print sensor values. Must send JSON Object."""
+    data = request.get_json(force=True)
+    print(data)
+    return data
+
 @app.route('/')
 def index():
-    """Main endpoint. Currently for testing."""
-    return "<h1>SenseHealth's Backend</h1>"
+    """Root endpoint. Currently for testing."""
+    return "<h1>SenseHealth's Backend</h1>Check out: \
+        https://5e9123ecf2fcfcdeef9cb85e--naughty-perlman-a4541f.netlify.com/"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
